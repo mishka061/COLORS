f38193fd841cfa85ede53477e80cb6815662c28e
Алена Котова <mishka061@rambler.ru>
2023-02-01 10:16:06 +0300

    функция оттенка цвета

--- app.js
+++ app.js
@@ -14,8 +14,6 @@ function generateRandomColor() {
     }
     return '#' + color
 }
-
-
 //создаем рандомные цвета для колонок
 //итерируем с помощью forEach стрелочной функцией
 //text- меняем цвет,который соответствует колонке
@@ -26,6 +24,21 @@ function setRandomColors() {
         const color = chroma.random()
         text.textContent = color
         col.style.background = generateRandomColor()
+        setTextColor(text, color)
     })
 }
+
+// функция оттенка цвета
+// передаем в библиотеку цвет,вызываем метод luminance
+// если luminance больше чем 0,5,то цвет черный ,иначе белый
+function setTextColor(text, color) {
+    const luminance = chroma(color).luminance()
+    text.style.color =luminance > 0.5 ? 'black' : 'white'
+}
+
+
+
+
+
+
 setRandomColors()
